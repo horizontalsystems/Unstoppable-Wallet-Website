46a9a3ae93de20a612bc6a6c37543d98d61130eb
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-01-02 18:31:40 +0600

    Initial commit and configure app

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App/App'
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+export default Routes
