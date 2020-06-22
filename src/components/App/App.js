f1a832ec6330b42f15725a467cabb7466682ebb2
iBK <8611762+esengulov@users.noreply.github.com>
2020-06-22 15:33:11 +0600

    wallet features text edits

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,47 +15,47 @@ function App() {
         <Home />
       </div>
       <div id="page-2" className="Page-dark">
-        <Page img="knowledge" text="Become informed and discover growing blockchain projects in early stages" rtl dark>
+        <Page img="knowledge" text="Become informed and discover promising cryptocurrencies at early stages" rtl dark>
           <Feature
-            icon="globe" title="Crypto News"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="globe" title="Learn"
+            info="Understand the ins and outs of storing and using cryptocurrency safely."
             rtl />
           <Feature
-            icon="guide" title="Crypto Guides"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="guide" title="Discover"
+            info="Learn about rising cryptocurrencies and projects in simple terms."
             rtl />
           <Feature
-            icon="graphic" title="Crypto Analytics"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="graphic" title="Monitor"
+            info="Get insights on when is a good to buy, hold or sell different assets."
             rtl />
         </Page>
       </div>
       <div id="page-3" className="Page-black">
         <Page img="independence" text="Keep crypto assets safe, unconfiscatable and accessible at all times">
           <Feature
-            icon="decentralized" title="Crypto News"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text." />
+            icon="non-costodial" title="Control"
+            info="Exercise full and exclusive control over your cryptocurrencies." />
           <Feature
-            icon="non-costodial" title="Non Costodial"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text." />
+            icon="open-source" title="Access"
+            info="Transact from anywhere at anytime without relying on any servers." />
           <Feature
-            icon="open-source" title="Free & Open Source"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text." />
+            icon="decentralized" title="Migrate"
+            info="Access your assets from any standard compliant crypto wallet." />
         </Page>
       </div>
       <div id="page-4" className="Page-dark">
         <Page img="incognito" text="Stay incognito and out of radar while growing your wealth" rtl>
           <Feature
-            icon="tor" title="TOR  integration"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="anonym" title="Dataless"
+            info="No accounts, emails, phone numbers or any other data collected."
             rtl />
           <Feature
-            icon="anonym" title="No Accounts"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="tor" title="Anonymous"
+            info="Anonymize internet and location while browsing the wallet app."
             rtl />
           <Feature
-            icon="sort" title="Input, Output sorting"
-            info="Contrary to popular belief, Lorem Ipsum is not simply random text."
+            icon="sort" title="Hidden"
+            info="Make cryptocurrency transactions harder to trace and link."
             rtl />
         </Page>
       </div>
