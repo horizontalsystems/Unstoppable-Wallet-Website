fbe9cb53a1123cf4b720e5ba836acd6ada43b7ae
iBK <8611762+esengulov@users.noreply.github.com>
2020-06-22 17:44:22 +0600

    fix links

--- src/components/GetWallet/GetWallet.js
+++ src/components/GetWallet/GetWallet.js
@@ -22,12 +22,12 @@ function GetWallet() {
             <List>
               <Button
                 text="Google Play" icon="google-play"
-                link="https://itunes.apple.com/app/bank-bitcoin-wallet/id1447619907?ls=1&mt=8" yellow />
-              <Button
-                text="App Store" icon="app-store"
                 link="https://play.google.com/store/apps/details?id=io.horizontalsystems.bankwallet" yellow />
               <Button
-                text="Download APK file" icon="download"
+                text="App Store" icon="app-store"
+                link="https://itunes.apple.com/app/bank-bitcoin-wallet/id1447619907?ls=1&mt=8" yellow />
+              <Button
+                text="Download APK" icon="download"
                 link="https://github.com/horizontalsystems/bank-wallet-android/releases" />
             </List>
           </div>
