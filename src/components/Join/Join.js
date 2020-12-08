98edc61c466d9a2b11ef8e55838ec58921883af0
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-12-08 11:32:10 +0600

    Update footer information

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -13,31 +13,30 @@ function Join() {
     <Container clipped={false}>
       <div className="Section-head">
         <div className="Section-text">
-          Join the Unstoppable <br/>
-          Wallet Community
+          The Unstoppable Wallet <br />Community Welcomes You
         </div>
         <div className="Section-info">
-          Use this wallet to remain incognito and be able to keep your information private
+          Keep up to date with Unstoppable related. You’ll be the first to know about new features, issues and more!
         </div>
         <div className="Section-join">
           <div className="Join-item">
             <a href="https://twitter.com/unstoppablebyhs" target="_blank" rel="noopener noreferrer">
-              <img className="Join-image" src={twitter} alt="Twitter"/>
+              <img className="Join-image" src={twitter} alt="Twitter" />
             </a>
           </div>
           <div className="Join-item">
             <a href="https://github.com/horizontalsystems/" target="_blank" rel="noopener noreferrer">
-              <img className="Join-image" src={github} alt="github"/>
+              <img className="Join-image" src={github} alt="github" />
             </a>
           </div>
           <div className="Join-item">
             <a href="https://t.me/unstoppable_announcements" target="_blank" rel="noopener noreferrer">
-              <img className="Join-image" src={telegram} alt="telegram"/>
+              <img className="Join-image" src={telegram} alt="telegram" />
             </a>
           </div>
           <div className="Join-item">
             <a href="https://www.reddit.com/r/UNSTOPPABLEWallet/" target="_blank" rel="noopener noreferrer">
-              <img className="Join-image" src={reddit} alt="reddit"/>
+              <img className="Join-image" src={reddit} alt="reddit" />
             </a>
           </div>
         </div>
