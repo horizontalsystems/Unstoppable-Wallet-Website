861d442304bc5864f09393bd19a49243cadd82bb
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-01-22 15:26:43 +0600

    Add links to Featured items

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -17,13 +17,27 @@ function Featured() {
       <div className="Featured">
         <div className="Featured-text">Featured In</div>
         <div className="Featured-list">
-          <img className="Featured-item" src={zcash} alt="ZCash" />
-          <img className="Featured-item" src={dash} alt="Dash" />
-          <img className="Featured-item" src={defiPulse} alt="Defi Pulse" />
-          <img className="Featured-item" src={walletConnect} alt="Wallet Connect" />
-          <img className="Featured-item" src={cryptoWisser} alt="Crypto Wisser" />
-          <img className="Featured-item" src={walletScrutiny} alt="Wallet Scrutiny" />
-          <img className="Featured-item" src={muo} alt="MakeUseOf" />
+          <a target="_blank" href="https://electriccoin.co/blog/zcash-is-unstoppable-privacy-focused-wallet-first-to-integrate-ecc-wallet-sdks/" rel="noopener noreferrer">
+            <img className="Featured-item" src={zcash} alt="ZCash" />
+          </a>
+          <a target="_blank" href="https://docs.dash.org/en/stable/wallets/third-party.html#unstoppable" rel="noopener noreferrer">
+            <img className="Featured-item" src={dash} alt="Dash" />
+          </a>
+          <a target="_blank" href="https://defipulse.com/defi-list" rel="noopener noreferrer">
+            <img className="Featured-item" src={defiPulse} alt="Defi Pulse" />
+          </a>
+          <a target="_blank" href="https://walletconnect.org/wallets" rel="noopener noreferrer">
+            <img className="Featured-item" src={walletConnect} alt="Wallet Connect" />
+          </a>
+          <a target="_blank" href="https://www.cryptowisser.com/wallet/unstoppable-wallet/" rel="noopener noreferrer">
+            <img className="Featured-item" src={cryptoWisser} alt="Crypto Wisser" />
+          </a>
+          <a target="_blank" href="https://walletscrutiny.com/android/io.horizontalsystems.bankwallet/" rel="noopener noreferrer">
+            <img className="Featured-item" src={walletScrutiny} alt="Wallet Scrutiny" />
+          </a>
+          <a target="_blank" href="https://www.makeuseof.com/tag/unstoppable-decentralized-crypto-wallet/" rel="noopener noreferrer">
+            <img className="Featured-item" src={muo} alt="MakeUseOf" />
+          </a>
         </div>
       </div>
     </Container>
