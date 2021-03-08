b9b6cd642d8bc6856558c500bb01a0f21fcb56b8
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-03-08 14:37:41 +0600

    Add link to Ethereum in the "Featured In" block

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 import Container from '../Container'
 import zcash from './zcash.svg'
-import dash from './dash.svg'
-import defiPulse from './defi-pulse.svg'
 import walletConnect from './wallet-connect.svg'
-import cryptoWisser from './crypto-wisser.svg'
 import bitcoin from './bitcoin.svg'
 import walletScrutiny from './wallet-scrutiny.svg'
-import muo from './muo.svg'
+import ethereum from './ethereum.svg'
 
 import './Featured.scss'
 
@@ -21,26 +18,17 @@ function Featured() {
           <a target="_blank" href="https://electriccoin.co/blog/zcash-is-unstoppable-privacy-focused-wallet-first-to-integrate-ecc-wallet-sdks/" rel="noopener noreferrer">
             <img className="Featured-item" src={zcash} alt="ZCash" />
           </a>
-          <a target="_blank" href="https://docs.dash.org/en/stable/wallets/third-party.html#unstoppable" rel="noopener noreferrer">
-            <img className="Featured-item" src={dash} alt="Dash" />
-          </a>
-          <a target="_blank" href="https://defipulse.com/defi-list" rel="noopener noreferrer">
-            <img className="Featured-item" src={defiPulse} alt="Defi Pulse" />
+          <a target="_blank" href="https://bitcoin.org/en/wallets/mobile/android/unstoppable/" rel="noopener noreferrer">
+            <img className="Featured-item" src={bitcoin} alt="Bitcoin" />
           </a>
           <a target="_blank" href="https://walletconnect.org/wallets" rel="noopener noreferrer">
             <img className="Featured-item" src={walletConnect} alt="Wallet Connect" />
           </a>
-          <a target="_blank" href="https://www.cryptowisser.com/wallet/unstoppable-wallet/" rel="noopener noreferrer">
-            <img className="Featured-item" src={cryptoWisser} alt="Crypto Wisser" />
-          </a>
-          <a target="_blank" href="https://bitcoin.org/en/wallets/mobile/android/unstoppable/" rel="noopener noreferrer">
-            <img className="Featured-item" src={bitcoin} alt="Bitcoin" />
+          <a target="_blank" href="https://ethereum.org/en/wallets/find-wallet/" rel="noopener noreferrer">
+            <img className="Featured-item" src={ethereum} alt="Ethereum" />
           </a>
           <a target="_blank" href="https://walletscrutiny.com/android/io.horizontalsystems.bankwallet/" rel="noopener noreferrer">
             <img className="Featured-item" src={walletScrutiny} alt="Wallet Scrutiny" />
-          </a>
-          <a target="_blank" href="https://www.makeuseof.com/tag/unstoppable-decentralized-crypto-wallet/" rel="noopener noreferrer">
-            <img className="Featured-item" src={muo} alt="MakeUseOf" />
           </a>
         </div>
       </div>
