beffdc28527571460500ffaf70c964c10b28916a
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-06-01 17:24:14 +0600

    Add avatar

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../Container'
+import Zooko from './zooko.png'
 
 function Quote() {
   return (
@@ -9,7 +10,10 @@ function Quote() {
           <p className="fs-5">
             “Unstoppable is the first multi-coin wallet that supports Bitcoin, Ethereum, and fully shielded Zcash, as well as other coins, and it has a strong, user-centric architecture in which the users own their own keys and their own privacy."
           </p>
-          <p className="text-grey fs-4 pt-5 mt-5">
+          <div className="pt-4">
+            <img src={Zooko} alt="Zooko Wilcox-O'Hearn" width="80" height="80" />
+          </div>
+          <p className="text-grey fs-4 pt-4">
             Zooko Wilcox-O'Hearn
           </p>
           <small className="text-grey">
