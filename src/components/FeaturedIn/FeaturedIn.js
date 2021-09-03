90e545157f3bfce29785cabc36595104f5cccefb
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-09-03 18:31:38 +0600

    Update Partners icons

--- src/components/FeaturedIn/FeaturedIn.js
+++ src/components/FeaturedIn/FeaturedIn.js
@@ -3,10 +3,14 @@ import React from 'react'
 import Container from '../Container'
 import zcash from './zcash.svg'
 import dash from './dash.svg'
+import defillama from './defillama.svg'
+import defiyield from './defiyield.svg'
+import coingecko from './coingecko.svg'
+import ens from './ens.svg'
+import blockchair from './blockchair.svg'
 import walletConnect from './wallet-connect.svg'
 import ethereum from './ethereum.svg'
 import oneInch from './1inch.svg'
-import defiPulse from './defi-pulse.svg'
 
 import './Featured.scss'
 
@@ -14,25 +18,37 @@ function FeaturedIn() {
   return (
     <Container>
       <div className="Featured py-5">
-        <div className="Featured-text">Featured In</div>
+        <div className="Featured-text">Ecosystem Partners</div>
         <div className="Featured-list">
           <a target="_blank" href="https://ethereum.org/en/wallets/find-wallet/" rel="noopener noreferrer">
             <img className="Featured-item" src={ethereum} alt="Ethereum" />
           </a>
-          <a target="_blank" href="https://walletconnect.org/wallets" rel="noopener noreferrer">
-            <img className="Featured-item" src={walletConnect} alt="Wallet Connect" />
+          <a target="_blank" href="https://blockchair.com/" rel="noopener noreferrer">
+            <img className="Featured-item" src={blockchair} alt="Blockchair" />
           </a>
           <a target="_blank" href="https://1inch.io/" rel="noopener noreferrer">
             <img className="Featured-item" src={oneInch} alt="1 Inch Network" />
           </a>
+          <a target="_blank" href="https://defillama.com/" rel="noopener noreferrer">
+            <img className="Featured-item" src={defillama} alt="Defi Llama" />
+          </a>
+          <a target="_blank" href="https://twitter.com/defiyield_app/status/1428469359756955663" rel="noopener noreferrer">
+            <img className="Featured-item" src={defiyield} alt="Defi Yield" />
+          </a>
+          <a target="_blank" href="https://www.coingecko.com/" rel="noopener noreferrer">
+            <img className="Featured-item" src={coingecko} alt="Coin Gecko" />
+          </a>
+          <a target="_blank" href="https://walletconnect.org/wallets" rel="noopener noreferrer">
+            <img className="Featured-item" src={walletConnect} alt="Wallet Connect" />
+          </a>
+          <a target="_blank" href="https://ens.domains/" rel="noopener noreferrer">
+            <img className="Featured-item" src={ens} alt="ENS" />
+          </a>
           <a target="_blank" href="https://docs.dash.org/en/stable/wallets/third-party.html#unstoppable" rel="noopener noreferrer">
             <img className="Featured-item" src={dash} alt="Dash" />
           </a>
-          <a target="_blank" href="https://electriccoin.co/blog/zcash-is-unstoppable-privacy-focused-wallet-first-to-integrate-ecc-wallet-sdks/" rel="noopener noreferrer">
+          <a target="_blank" href="https://z.cash/wallets/" rel="noopener noreferrer">
             <img className="Featured-item" src={zcash} alt="ZCash" />
-          </a>
-          <a target="_blank" href="https://defipulse.com/defi-list/" rel="noopener noreferrer">
-            <img className="Featured-item" src={defiPulse} alt="Defi Pulse" />
           </a>
         </div>
       </div>
