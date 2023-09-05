ea60aacdbcc4cecaaee5889cd547de1cf708e6c0
Talgat Medetbekov <medetbekov.t@gmail.com>
2023-09-05 12:35:28 +0600

    Update the active step icon in the Roadmap

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -3,7 +3,7 @@ import cn from 'classnames'
 import Slider from 'react-slick'
 import Markdown from '../Markdown/Markdown'
 import Container from '../Container'
-import Circle from './circle.svg'
+import { ReactComponent as Circle } from './circle.svg'
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -127,7 +127,7 @@ export function RoadmapItem({ isActive, date, version, onClick, isStart, isEnd }
       <div className="Roadmap-progress" />
       <div className={cn('Roadmap-circle', { 'Roadmap-circle-active': isActive })}>
         <div className="text-grey small">{date}</div>
-        <img src={Circle} alt="" width={width} height={height} role="button" />
+        <Circle width={width} height={height} role="button" />
         <div className="text-steel-light">Version {version}</div>
       </div>
     </div>
