facbf0013eaa98796da9123931af92ce880778e3
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-04-19 16:00:22 +0600

    Collapse navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,37 @@
+'use client'
+
 import Image from 'next/image'
+import cn from 'classnames'
+import { useState } from 'react'
 
 export default function Header() {
+  const [show, setShow] = useState(false)
+
   return (
     <nav className="Header-navbar navbar navbar-expand-lg fixed-top">
       <div className="container">
         <a className="navbar-brand" href="#">
           <Image className="Header-logo" src="/header-logo.svg" alt="Logo" width={142} height={54} priority />
         </a>
-        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false">
+        <button className="navbar-toggler" onClick={() => setShow(!show)}>
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbar">
+        <div className={cn('collapse navbar-collapse', { show })}>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Academy</a>
+              <a className="nav-link small text-white active" aria-current="page" href="#">Academy</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link small text-white" href="#">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">FAQ</a>
+              <a className="nav-link small text-white" href="#">FAQ</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Github</a>
+              <a className="nav-link small text-white" href="#">Github</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Gitlab</a>
+              <a className="nav-link small text-white" href="#">Gitlab</a>
             </li>
           </ul>
         </div>
