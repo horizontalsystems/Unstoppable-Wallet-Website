7173bd2d0c8ae837f12c13fb3bf66973ff648297
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-05-03 17:05:45 +0600

    Improve sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 'use client'
 
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { debounce } from 'next/dist/server/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import cn from 'classnames'
@@ -8,10 +9,39 @@ import cn from 'classnames'
 import './Header.css'
 
 export function Header() {
-  const [show, setShow] = React.useState(false)
+  const [show, setShow] = useState(false)
+  const [prevScrollTop, setPrevScrollTop] = useState(0)
+  const [isVisible, setIsVisible] = useState(true)
+  const [isInBottom, setIsInBottom] = useState(false)
+
+  const headerRef = useRef(null)
+  const className = cn('Header navbar navbar-expand-md fixed-top', {
+    'is-hidden': !isVisible,
+    'is-in-bottom': isInBottom
+  })
+
+  useEffect(() => {
+    const header = headerRef.current
+
+    const onScroll = () => {
+      const currScrollTop = document.documentElement.scrollTop
+      const headerHeight = header.clientHeight
+
+      setIsVisible(currScrollTop < headerHeight || currScrollTop <= prevScrollTop)
+      setIsInBottom(currScrollTop > headerHeight)
+
+      setPrevScrollTop(currScrollTop)
+    }
+
+    const handleScroll = debounce(onScroll, 100)
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollTop])
 
   return (
-    <nav className="Header navbar navbar-expand-md fixed-top">
+    <nav className={className} ref={headerRef}>
       <div className="container">
         <Link className="navbar-brand" href="/">
           <Image className="Header-logo" src="/header-logo.svg" alt="Logo" width={142} height={54} priority />
