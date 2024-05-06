ed9b530bc968e86d49e694bc4c53efe0d295f3e7
Talgat Medetbekov <medetbekov.t@gmail.com>
2024-05-06 14:39:42 +0600

    Hide tickers

--- src/components/Home.js
+++ src/components/Home.js
@@ -273,48 +273,6 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="ticker-wrap">
-          <div className="ticker">
-              <span className="item-collection-1">
-                <span className="ticker-item">
-                  <Image src="/images/tickers/ethereum.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/blockchair.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/1inch.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/defillama.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/defiyield.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/coingecko.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/ens.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/wc.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/chainlink.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/dash.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/zcash.png" alt="" width="220" height="80" />
-                </span>
-                <span className="ticker-item">
-                  <Image src="/images/tickers/alpaca.png" alt="" width="220" height="80" />
-                </span>
-              </span>
-          </div>
-        </div>
       </Page>
 
       <Page darkest>
