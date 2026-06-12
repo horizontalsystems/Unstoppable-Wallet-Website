75001812c0900910d271d22ab33563b2726704a0
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-06-12 17:36:12 +0600

    Add release md files to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  outputFileTracingIncludes: {
+    '/releases': ['./public/releases/**/*.md'],
+  },
 };
 
 export default nextConfig;
